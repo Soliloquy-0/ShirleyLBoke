341d0c1459b25d8f7987aa36112bd4a2687720c2
Soliloquy-0 <78271154+Soliloquy-0@users.noreply.github.com>
2025-01-27 22:19:39 -0800

    526HW1PLANE

---
+++ My project (2)/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset= new Vector3(20, 3, 9);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+        // 固定摄像机的旋转角度，不随玩家旋转
+        transform.rotation = Quaternion.Euler(0, -90, 0);
+    }
+}
